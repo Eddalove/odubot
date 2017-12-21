a454668c53e0c42725522e23053c633f5f6779d6
Odunita <34730713+Odunita@users.noreply.github.com>
2017-12-20 21:38:46 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,23 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
+const token = "MzkzMjQxNjM3MjM0NDA5NDcy.DRzROw.2uWjSNr98rJ56Xg48oSxXWGtjgQ";
+const prefix = "\" 
 
-client.on("ready", () => {
-    console.log('Odunita at your service.');
+
+
+bot.on("ready", async () => {
+console.log(`Odunita at your service`);
+});
+//ready event
+
+
+bot.on("message", async message => {
+if(message.author.bot) return;
+//ignores all messages from other bots
+if(message.channel.type === "dm") return;
+//explained by itself
+if(message.content === prefix + "ping") {
+message.channel.send("pong!");
 });
 
-const p = "\";
-client.on('message', msg => {
-    if(msg.content === (p + "ping")) {
-        msg.channel.sendMessage("Pong?");
-    }
-});
-
-client.login('MzkzMjQxNjM3MjM0NDA5NDcy.DRzROw.2uWjSNr98rJ56Xg48oSxXWGtjgQ');
+bot.login(token);
